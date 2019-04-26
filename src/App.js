1379319946127a30924530cd4f343911a9c76118
usnaik <upendra_naik@yahoo.com>
2019-04-26 23:27:31 +0530

    updates to travis and package.json

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          {/* Hi there! */}
         </p> 
         <a
           className="App-link"
